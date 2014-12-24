fb4b8c351a17e097956e5fe06444fb6f0d40490b
Jürgen Viljaste <j.viljaste@gmail.com>
2014-12-25 01:59:14 +0200

    Update

--- src/build/modules/s3fs/manifests/init.pp
+++ src/build/modules/s3fs/manifests/init.pp
@@ -1,3 +1,32 @@
 class s3fs {
+  file { '/tmp/s3fs-fuse-1.78.tar.gz':
+    ensure => present,
+    source => 'puppet:///modules/s3fs/tmp/s3fs-fuse-1.78.tar.gz'
+  }
 
+  exec { 'tar xzf s3fs-fuse-1.78.tar.gz':
+    cwd => '/tmp',
+    path => ['/bin'],
+    require => File['/tmp/s3fs-fuse-1.78.tar.gz']
+  }
+
+  exec { '/bin/su - root -c "cd /tmp/s3fs-fuse-1.78 && ./autogen.sh"':
+    timeout => 0,
+    require => Exec['tar xzf s3fs-fuse-1.78.tar.gz']
+  }
+
+  exec { '/bin/su - root -c "cd /tmp/s3fs-fuse-1.78 && ./configure --prefix=/usr"':
+    timeout => 0,
+    require => Exec['/bin/su - root -c "cd /tmp/s3fs-fuse-1.78 && ./autogen.sh"']
+  }
+
+  exec { '/bin/su - root -c "cd /tmp/s3fs-fuse-1.78 && make"':
+    timeout => 0,
+    require => Exec['/bin/su - root -c "cd /tmp/s3fs-fuse-1.78 && ./configure --prefix=/usr"']
+  }
+
+  exec { '/bin/su - root -c "cd /tmp/s3fs-fuse-1.78 && make install"':
+    timeout => 0,
+    require => Exec['/bin/su - root -c "cd /tmp/s3fs-fuse-1.78 && make"']
+  }
 }
