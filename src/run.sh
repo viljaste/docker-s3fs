72aecfbaa26daf3713bb4871a80b389f26fcb041
Jürgen Viljaste <j.viljaste@gmail.com>
2014-12-25 12:48:27 +0200

    Update

--- src/run.sh
+++ src/run.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 export FACTER_BUCKET="${BUCKET}"
+export FACTER_AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}"
+export FACTER_AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}"
 
 puppet apply --modulepath=/src/run/modules /src/run/run.pp
 
--- src/run/modules/s3fs/manifests/init.pp
+++ src/run/modules/s3fs/manifests/init.pp
@@ -1,5 +1,12 @@
 class s3fs {
-  exec { "s3fs $bucket /s3fs/data":
-    path => ['/usr/bin']
+  file { '/etc/passwd-s3fs':
+    ensure => present,
+    content => template('s3fs/passwd-s3fs'),
+    mode => 640
+  }
+
+  exec { "s3fs $bucket /s3fs/data -ouse_cache=/tmp":
+    path => ['/usr/bin'],
+    require => File['/etc/passwd-s3fs']
   }
 }
